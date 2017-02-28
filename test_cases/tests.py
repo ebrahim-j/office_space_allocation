b6a66de599bf959b0491c98a762b10cbe513fb8e
mnjaggah <maria.njaggah@gmail.com>
2017-03-01 01:56:49 +0300

    Restructure testcases

--- test_cases/tests.py
+++ test_cases/tests.py
@@ -8,26 +8,54 @@ class TestCreateRoom(unittest.TestCase):
 		self.living_space = LivingSpace()
 
 
-	def test_office_creation(self):
+	def test_officespace_creation(self):
+		initial_count_offices = len(self.office_space.total)
+		self.office_space.create_new("Office 1")
+		new_count_offices = len(self.office_space.total) 
+		self.assertEqual(new_count_offices - initial_count_offices, 1, 
+			msg = 'No new office created')
+	
 
-		self.office_space = OfficeSpace("Office 1")
-		self.assertIsInstance(self.office_space, OfficeSpace,
-			msg = "'office_space' should be an instance of 'OfficeSpace' class")
-
-		office_capacity = self.office_space.size
-		self.assertEqual(office_capacity , 6 ,
+	def test_officespace_size_type(self):
+		self.assertEqual(self.office_space.size , 6 ,
 			msg ='Maximum capacity is 6 occupants')
+
+		self.assertEqual(self.office_space.room_type, "OFFICE",
+			msg = "Room created should be of type OFFICE")
+
+
+	def test_officespace_accepts_str(self):
+		self.assertRaises(ValueError, self.office_space.create_new, 111)
+
+
 
 	def test_livingspace_creation(self):
 
-		self.living_space = LivingSpace("Rodgen")
-		self.assertIsInstance(self.living_space, LivingSpace,
-			msg = "'living_space' should be an instance of 'LivingSpace' class")
-		)
-		living_capacity = self.living_space.size
-		self.assertEqual(living_capacity, 4,
-			msg = 'Maximum capacity is 4 occupants')
+		initial_count_ls = len(self.living_space.total)
+		self.living_space.create_new("Rodgen")
+		new_count_ls = len(self.office_space.total)
+		self.assertEqual(new_count_ls - initial_count_ls , 1, 
+			msg = 'No new Living space created')
 
 
+	def test_livingspace_size_type(self):
+
+		self.assertEqual(self.living_space.size, 4, msg = 'Maximum capacity is 4 occupants')
+
+		self.assertEqual(self.living_space.room_type, "LIVING SPACE",
+			msg = "Room Created should be of type LIVING SPACE")
+
+	
+	def test_livingspace_accepts_str(self):
+		self.assertRaises(ValueError, self.living_space.create_new, 111)
 
 
+class TestPerson(unittest.TestCase):
+	"""Test Person added"""
+
+	def setUp(self):
+		self.new_fellow = Fellow()
+		self.new_staff = Staff()
+
+	def test_adding_fellow(self):
+		self.new_fellow.add()
