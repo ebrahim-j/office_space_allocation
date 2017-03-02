5d7a47aff21ca8533e605a9aa9adf76a175da69e
mnjaggah <maria.njaggah@gmail.com>
2017-03-02 13:21:18 +0300

    Add class definition structure

---
+++ app/room.py
@@ -0,0 +1,16 @@
+class Room (object):
+	
+	def __init__(self, room_name, room_type):
+		self.room_name = room_name
+		self.room_type = room_type
+		pass
+
+class OfficeSpace(Room):
+
+	def __init__(self):
+		pass
+
+class LivingSpace(Room):
+
+	def __init__(self):
+		pass
