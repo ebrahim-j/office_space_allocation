83bbc1c7ccde4fd16a94ad6fa658049b9eef18ab
mnjaggah <maria.njaggah@gmail.com>
2017-03-06 08:05:11 +0300

    Add functionality to add_person and allocate room

--- app/dojo.py
+++ app/dojo.py
@@ -1,7 +1,7 @@
-"""This file defines all functions for our office space allocation app"""
 from person import Person, Fellow, Staff
 from room import OfficeSpace, LivingSpace
 
+"""This file defines all functions for our office space allocation app"""
 
 class Dojo(object):
 
@@ -11,17 +11,18 @@ class Dojo(object):
 		self.all_fellows = []
 		self.all_staff = []
 
+
 	def create_room(self, rm_type, rm_names):
 		"""Create a new room, either office or living space at the Dojo. 
 		 Check whether a room with a similar name exits,
 		 before succesfully creating the room
 		 """
-		if rm_type.lower() == "office"	
+		if rm_type.lower() == "office":	
 			for i in rm_names:
 				if isinstance(i,str):
 					name = i.lower()
-					if name in [office.name for office in self.all_offices]:
-					return ("This office already exists!")
+					if name in [office.name for office in self.all_offices]:	#checks if an instance of this room exists
+						return ("This office already exists!")
 					new_office = OfficeSpace(name)
 					self.all_offices.append(new_office)
 					return ("An {} called {} has been successfully created" .format(rm_type, name))
@@ -38,26 +39,68 @@ class Dojo(object):
 					return ("A {} called {} has been successfully created" .format(rm_type, name))
 				else:
 					return ("'room_name' should be a string value!")
+
 				
-	def add_person(self, name, role, wants_accomodation='N'):
+	def add_person(self, name, role, wants_accomodation="N"):
 		"""Add a new person to the Dojo and allocate 
 		either office space or living space 
 		"""
-		if role.lower() == "staff":
-			new_staff = Staff(name)
-			self.all_staff.append(new_staff)
-			print (" {} {} has been successfully added" .format(role, name))
-			self.allocate_room(new_staff)
-		elif role.lower() == "fellow":
-			new_fellow = Fellow(name, wants_accomodation)
-			self.all_fellows.append(new_fellow)
-			print ("{} {} has been successfully added" .format(rm_type, rm_name))
-			self.allocate_room(new_fellow)
+		if wants_accomodation.lower() != "Y" or wants_accomodation.lower() != "N":
+			return ("Invalid accomodation choice")
+		#if checks the role against accomodation option to allocate respective room
+		if wants_accomodation.lower() == "y":
+			if role.lower() == "fellow":
+				new_fellow = Fellow(name, wants_accomodation)
+				self.all_fellows.append(new_fellow)
+				return ("{} {} has been successfully added" .format(rm_type, rm_name))
+				self.allocate_available_officespace(new_fellow)
+				self.allocate_available_livingspace(new_fellow)
+			elif role.lower() == "staff":
+				return "Staff cannot be assigned Living Space"
+		elif wants_accomodation.lower() == "n":
+			if role.lower() == "staff":
+				new_staff = Staff(name)
+				self.all_staff.append(new_staff)
+				return (" {} {} has been successfully added" .format(role, name))
+				self.allocate_available_officespace(new_staff)
+			elif role.lower() == "fellow":
+				new_fellow = Fellow(name)
+				self.all_fellows.append(new_fellow)
+				return ("{} {} has been successfully added" .format(rm_type, rm_name))
+				self.allocate_available_officespace(new_fellow)
+				self.allocate_available_livingspace(new_fellow)
+				
+			
+	def allocate_available_officespace(self,person):
+		"""This method gets all available offices, confirms if the
+		room has space then allocates an office randomly to either fellow or staff
+		"""
+		available_office = []
+
+		for of in self.all_offices:
+			if len(of.occupants) < OfficeSpace.capacity:
+				available_office.append(of)
+		#loop to check office space to be allocated exists
+		if available_office >= 1
+			random_office_space = random.choice(available_office)
+			random_office_space.occupants.append(person)
+			return ("{} has been allocated the office {} " .format(person.name, random_office_space))
 		else:
-			return ("You can only be staff or fellow at the Dojo")
+			return ("No available office space")
 
-	def allocate_room(person):
-		pass
-	
-
-
+	def allocate_available_livingspace(self, person):
+		"""This method gets all available living space, confirms if the
+		room has space then allocates living space randomly to fellow who requires
+		accomodation
+		"""
+		available_livingspace = []
+		for living in self.all_livingspace:
+			if len(living.occupants) < LivingSpace.capacity:
+				available_livingspace.append(living)
+		#loop checks vacant livingspace to be allocated
+		if available_livingspace >= 1
+			random_living_space= random.choice(available_livingspace)
+			random_living_space.occupants.append(person)
+			return ("{} has been allocated the livingspace {}" .format(person.name, random_living_space))
+		else:
+			return ("No available living space")
