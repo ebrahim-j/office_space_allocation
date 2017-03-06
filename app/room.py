801b264f13a62d0502cf9c980164e15b8439bcd3
mnjaggah <maria.njaggah@gmail.com>
2017-03-06 07:57:10 +0300

    Add list variable to hold allocated occupants

--- app/room.py
+++ app/room.py
@@ -3,14 +3,24 @@ class Room (object):
 	def __init__(self, room_name, room_type):
 		self.room_name = room_name
 		self.room_type = room_type
-		pass
+		
+
+#implement all methods that are similar to these two classes
 
 class OfficeSpace(Room):
 
-	def __init__(self):
-		pass
+	def __init__(self, name):
+		self.name = name
+		self.room_type = "OFFICE"
+		self. capacity = 6
+		self.occupants = []
+		
 
 class LivingSpace(Room):
 
-	def __init__(self):
-		pass
+	def __init__(self, name):
+		self.name = name
+		self.room_type = "LIVING SPACE"
+		self.capacity = 4
+		self.occupants = []
+		
