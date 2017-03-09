9732ec7ac028e19ede733e5034f1c6bad542474b
mnjaggah <maria.njaggah@gmail.com>
2017-03-09 03:15:29 +0300

    Add unittests for task1 functionalities

--- test_cases/test_dojo.py
+++ test_cases/test_dojo.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+
 from ..app.dojo import Dojo
 
 
@@ -16,21 +18,21 @@ class TestDojoFunctionalities(unittest.TestCase):
 
 	def test_livingspace_creation(self):
 		initial_count_livingspace = len(self.the_dojo.all_livingspace)
-		self.the_dojo.create_room("living space", "Emerald")
+		self.the_dojo.create_room("livingspace", "Emerald")
 		new_count_livingspace = len(self.the_dojo.all_livingspace)
 		self.assertEqual(new_count_livingspace - initial_count_livingspace, 1, 
 			msg = 'No new living space created')
 
 	def test_adding_fellow(self):
 		initial_count_fellows = len(self.the_dojo.all_fellows)
-		self.the_dojo.add_person("Barney", "FELLOW", "N")
+		self.the_dojo.add_person("Barney", "barney@barney.com","FELLOW", "N")
 		new_count_fellows= len(self.the_dojo.all_fellows)
 		self.assertEqual(new_count_fellows - initial_count_fellows, 1, 
 			msg = 'Failed to add new Fellow')		
 
 	def test_adding_staff(self):
 		initial_count_staff = len(self.the_dojo.all_staff)
-		self.the_dojo.add_person("Louis", "STAFF")
+		self.the_dojo.add_person("Louis", "louis@louis", "Staff")
 		new_count_staff= len(self.the_dojo.all_staff)
 		self.assertEqual(new_count_staff - initial_count_staff, 1, 
 			msg = 'Failed to add new Staff')
@@ -43,4 +45,34 @@ class TestDojoFunctionalities(unittest.TestCase):
 		with self.assertRaises(ValueError):
 			self.the_dojo.create_room("OFFICE",123)
 	
+	def test_that_a_room_exists_before_printing_occupants(self):
+		self.the_dojo.create_room("office","Green")
+		self.assertTrue(self.the_dojo.print_room("Green"))
+
+	def test_print_room_returns_error_message_if_room_doesnot_exist(self):
+		self.the_dojo.create_room("office","blue")
+		self.assertEqual(self.the_dojo.print_room("red"), "This room doesnot exist!")
+
+	def test_print_room_prints_all_occupants_if_room_is_office(self):
+		self.the_dojo.create_room("office","Purple")
+		self.the_dojo.add_person("Pete","pete@pete","Staff")
+		self.assertEqual(self.the_dojo.print_room("Purple"), "Pete")
+
+	def test_print_room_prints_all_occupants_if_room_is_livingspace(self):
+		self.the_dojo.create_room("livingspace", "Ruby")
+		self.the_dojo.add_person("Marie", "marie@marie.com","Fellow", "y")
+		self.assertEqual(self.the_dojo.print_room("Ruby"), "Marie")
+
+	def test_if_prints_allocated_filepath_exists(self):
+		self.the_dojo.print_allocations("file1")
+		self.assertTrue(os.path.isfile("file1.txt"))
+		os.remove("file1.txt")
+
+	def test_it_print_unallocated_filepath_exists(self):
+		self.the_dojo.print_unallocated("file2")
+		self.assertTrue(os.path.isfile("file2.txt"))
+		os.remove("file2.txt")
+		
+
+
 
