e7213f1798daa43e7fba277713e4fcaae7a62d2d
mnjaggah <maria.njaggah@gmail.com>
2017-03-14 18:01:04 +0300

    Add unittests for reallocating and loading people from file

--- test_cases/test_dojo.py
+++ test_cases/test_dojo.py
@@ -48,17 +48,18 @@ class TestDojoFunctionalities(unittest.TestCase):
 	def test_print_room_returns_error_message_if_room_is_empty(self):
 		self.the_dojo.create_room("office","Green")
 		output = self.the_dojo.print_room("Green")
-		self.assertEqual(output, "The OFFICE Green has no occupants")
+		self.assertEqual(output, "\n\n\tThe OFFICE Green has no occupants\n\n")
 
 	def test_print_room_returns_error_message_if_room_doesnot_exist(self):
 		self.the_dojo.create_room("office","blue")
-		self.assertEqual(self.the_dojo.print_room("red"), "The room red does not exist!")
+		self.assertEqual(self.the_dojo.print_room("red"),
+		 "\n\tThe room red does not exist!\n")
 
 	def test_print_room_prints_all_occupants_if_room_is_office(self):
 		self.the_dojo.create_room("office","Purple")
 		self.the_dojo.add_person("Pete","pete@pete","Staff")
 		output = self.the_dojo.print_room("Purple")
-		expected_output =("\n LIST OF ALL OCCUPANTS IN OFFICE Purple\n" + "*" * 40 
+		expected_output =("\n LIST OF ALL OCCUPANTS IN OFFICE Purple\n" + "*" * 50 
 			+ "\n" + "Pete" + "\t" + "STAFF" + "\n")
 		self.assertEqual(output, expected_output)
 
@@ -66,8 +67,9 @@ class TestDojoFunctionalities(unittest.TestCase):
 		self.the_dojo.create_room("livingspace", "Ruby")
 		self.the_dojo.add_person("Marie", "marie@marie.com","Fellow", "y")
 		output = self.the_dojo.print_room("Ruby")
-		expected_output = ("\n LIST OF ALL OCCUPANTS IN LIVING SPACE Ruby\n" + "*" * 40 + "\n" 
-			+ "Marie" + "\t" + "FELLOW" + "\n")
+		expected_output = ("\n LIST OF ALL OCCUPANTS IN LIVING SPACE Ruby\n" 
+			+ "*" * 40 + "\n" 
+			+ "\n" + "Marie" + "\t" + "FELLOW" + "\n")
 		self.assertEqual(output, expected_output)
 
 	def test_print_allocations_with_filename_unspecified(self):
@@ -85,7 +87,8 @@ class TestDojoFunctionalities(unittest.TestCase):
 		self.the_dojo.add_person("Marie", "marie@marie.com","Fellow", "y")
 		self.the_dojo.add_person("Pete","pete@pete","Staff")
 		output = self.the_dojo.print_allocations("file1")
-		expected_output = "\nData has been successfully saved to file1.txt\n"
+		expected_output = "Saving output data to file...\
+		\nData has been successfully saved to file1.txt\n"
 		self.assertEqual(output, expected_output)
 
 	def test_print_allocations_if_filepath_exists(self):
@@ -97,16 +100,19 @@ class TestDojoFunctionalities(unittest.TestCase):
 		self.the_dojo.add_person("Marie", "marie@marie.com","Fellow", "y")
 		self.the_dojo.add_person("Pete","pete@pete","Staff")
 		output = self.the_dojo.print_unallocated()
-		expected_output = ("\n\n\tUNALLOCATED PEOPLE\n" + "*" * 50 
-			+ "\n\tMarie\tmarie@marie.com\tFellow\ty \n" 
-			+ "\n\tPete\tpete@pete.com\tStaff ")
+		expected_output = ("\n\n LIST OF ALL UNALLOCATED STAFF AND FELLOWS\n" 
+			+ "*" * 50 
+			+ "\n\tMarie\tmarie@marie.com\tFellow\tOFFICE SPACE\n" 
+			+ "\n\tPete\tpete@pete.com\tStaff\tOFFICE SPACE\n"
+			+ "\n\tMarie\tmarie@marie.com\tFellow\tLIVING SPACE\n" )
 		self.assertEqual(output, expected_output)
 			
 	def test_print_unallocated_with_filename_specified(self):
 		self.the_dojo.add_person("Marie", "marie@marie.com","Fellow", "y")
 		self.the_dojo.add_person("Pete","pete@pete","Staff")
-		output = self.the_dojo.print_unallocated("file1")
-		expected_output = "\nData has been successfully saved to file2.txt\n"
+		output = self.the_dojo.print_unallocated("file2")
+		expected_output = "Saving unallocations list to file...\
+		\nData has been successfully saved to file2.txt\n"
 		self.assertEqual(output, expected_output)
 
 	def test_if_print_unallocated_filepath_exists(self):
@@ -114,14 +120,116 @@ class TestDojoFunctionalities(unittest.TestCase):
 		self.assertTrue(os.path.isfile("file2.txt"))
 		os.remove("file2.txt")
 
-	def test_reallocate_person_successfully(self):
+	def test_reallocate_person_returns_error_if_room_doesnot_exists(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Pete", "pete@pete", "staff")
+		result =self.the_dojo.reallocate_person("pete@pete", "Green")
+		self.assertEqual(result, "The OFFICE Green doesnot exist!")
 
-		pass
+	def test_reallocate_person_moves_person_successfully_to_specified_room(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Pete", "pete@pete", "staff")
+		self.the_dojo.create_room("office", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Green"]
+		count_before_reallocation = len(room_occupancy[0])
+		self.the_dojo.reallocate_person("pete@pete", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Green"]
+		count_after_reallocation = len(room_occupancy[0])
+		self.assertEqual(count_after_reallocation - count_before_reallocation, 1, 
+			msg = "Reallocation was unsuccessful!" )
+
+	def test_reallocate_person_moves_person_from_previous_allocation(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Pete", "pete@pete", "staff")
+		self.the_dojo.create_room("office", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Red"]
+		count_before_reallocation = len(room_occupancy[0])
+		self.the_dojo.reallocate_person("pete@pete", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Red"]
+		count_after_reallocation = len(room_occupancy[0])
+		self.assertEqual(count_before_reallocation - count_after_reallocation, 1, 
+			msg = "Reallocation was unsuccessful!" )
+		
+	def test_reallocate_person_doesnot_reallocate_staff_to_livingspace(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Pete", "pete@pete", "staff")
+		self.the_dojo.create_room("livingspace", "Crimson")
+		result = self.the_dojo.reallocate_person("pete@pete", "Crimson")
+		self.assertEqual(result, "Cannot reallocate STAFF to LIVING SPACE!")
+
+	def tese_reallocate_person_doesnot_reallocate_if_room_is_full(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Pete", "pete@pete", "staff")
+		self.the_dojo.add_person("Matt", "matt@matt", "staff")
+		self.the_dojo.add_person("Patt", "patt@patt", "fellow")
+		self.the_dojo.add_person("Faith", "faith@faith", "staff")
+		self.the_dojo.add_person("Fait", "fait@fait", "fellow", "y")
+		self.the_dojo.add_person("Fai", "fai@fai", "staff")
+		self.the_dojo.create_room("office", "Green")
+		self.the_dojo.add_person("Marie", "maz@maz", "fellow","y")
+		result = self.the_dojo.reallocate_person("maz@maz", "Red")
+		self.assertEqual(result, "The room Red is full! Cannot reallocate FELLOW-Marie")
+
+	def test_reallocate_person_can_reallocate_a_fellow(self):
+		self.the_dojo.create_room("office", "Red")
+		self.the_dojo.add_person("Josh", "josh@josh", "fellow", "y")
+		self.the_dojo.create_room("office", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Green"]
+		count_before_reallocation = len(room_occupancy[0])
+		self.the_dojo.reallocate_person("josh@josh", "Green")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_offices\
+		 if room.name == "Green"]
+		count_after_reallocation = len(room_occupancy[0])
+		self.assertEqual(count_after_reallocation - count_before_reallocation, 1, 
+			msg = "Reallocation was unsuccessful!" )
+
+	def test_reallocate_person_successfully_reallocates_fellow_to_livingspace(self):
+		self.the_dojo.create_room("office","Red")
+		self.the_dojo.create_room("livingspace", "Crimson")
+		self.the_dojo.add_person("Matt","matt@matt", "fellow", "y")
+		self.the_dojo.create_room("livingspace","Emerald")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_livingspace\
+		 if room.name == "Emerald"]
+		count_before_reallocation = len(room_occupancy[0])
+		self.the_dojo.reallocate_person("matt@matt", "Emerald")
+		room_occupancy = [room.occupants for room in self.the_dojo.all_livingspace\
+		 if room.name == "Emerald"]
+		count_after_reallocation = len(room_occupancy[0])
+		self.assertEqual(count_after_reallocation - count_before_reallocation, 1, 
+			msg = "Reallocation was unsuccessful!" )
 
 	def test_load_people_successfully(self):
+		with open("LoadFile", "w+") as input_file:
+			input_file.write("RANDY RANDY@RANDY STAFF\n")
+			input_file.write("TYRESE TY@TY FELLOW Y")
+			input_file.write("HEATHER HEATH@HEATH FELLOW")
 
-		pass
+		initial_staff_count = self.the_dojo.all_staff
+		initial_fellow_count = self.the_dojo.all_fellows
+		self.the_dojo.load_people("LoadFile")
+		final_staff_count = self.the_dojo.all_staff
+		final_fellow_count = self.the_dojo.all_fellows
+		self.assertEqual(final_staff_count - initial_staff_count, 1,
+			msg = "Error loading Staff")
+		self.assertEqual(final_fellow_count - initial_fellow_count, 2,
+			msg = "Error Loading Fellows")
 
+	def test_load_people_returns_error_if_file_doesnot_exist(self):
+		result = self.the_dojo.load_people("LoadFile")
+		self.assertEqual(result, "The file LoadFile doesnot exist!")
+
+
+
+	def test_load_people_returns_error_if_file_is_empty(self):
+		text_file = open("LoadFile" + ".txt", "w+")
+		text_file.close()
+		result = self.the_dojo.load_people("Loadfile.txt")
+		self.assertEqual(result, "The file LoadFile.txt is empty!")
 
 
 
